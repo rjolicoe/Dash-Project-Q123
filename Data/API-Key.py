657723e641637bbeb82a7aac3ca0be2aa89290c4
Jolicoeur, Ryan Marc <rjolicoeur3@gatech.edu>
2023-01-28 13:39:17 -0700

    remove API-Key.py

--- Data/API-Key.py
+++
@@ -1,7 +0,0 @@
-import keyring
-
-if __name__ == '__main__':
-    keyring.set_password("X-RapidAPI-Host", "X-RapidAPI-Key",
-                         "020f2b0544mshb231f7939bd760fp171d50jsnca2dce7a9b39")
-
-    api_key = keyring.get_password("X-RapidAPI-Host", "X-RapidAPI-Key")
